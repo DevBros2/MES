29fb2dcad50dc6f4a9e30e53c1830043e3bd69c8
Bonga Ngcobo <Bonga.Developer@outlook.com>
2018-08-13 07:47:01 +0200

    bottom navbar was added.

--- MES/MainActivity.cs
+++ MES/MainActivity.cs
@@ -15,39 +15,33 @@ namespace MES
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
-
+            
+            // Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.activity_main);
 
-			Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            var bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
+            bottomNavigation.NavigationItemSelected += (s, e) =>
+            {
+                switch (e.Item.ItemId)
+                {
+                    case Resource.Id.action_add:
+                        Toast.MakeText(this, "Action add Clicked", ToastLength.Short).Show();
+                        break;
 
-			FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+                    case Resource.Id.action_edit:
+                        Toast.MakeText(this, "Action edit Clicked", ToastLength.Short).Show();
+                        break;
+
+                    case Resource.Id.action_remove:
+                        Toast.MakeText(this, "Action remove Clicked", ToastLength.Short).Show();
+                        break;
+
+                }
+            };
+
+			
 		}
 
-		public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
-            {
-                return true;
-            }
-
-            return base.OnOptionsItemSelected(item);
-        }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }
 	}
 }
 
